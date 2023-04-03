5e9bf5a8f8241855119bb057f98af056dccd5a44
ws-sumo <129716148+ws-sumo@users.noreply.github.com>
2023-04-03 17:25:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,13 +11,13 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/icon.jpg',
+  icon: '/icon/icon.jpg',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "古月轩",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/共享网盘',
