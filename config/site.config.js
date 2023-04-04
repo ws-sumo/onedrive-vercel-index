9d36999c07c56a3ac8cc2b1145e3c7bc46775fa5
ws-sumo <129716148+ws-sumo@users.noreply.github.com>
2023-04-04 16:28:43 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/icon.png',
+  icon: '/共享网盘/icons/1.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
