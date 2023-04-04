1b7fc12b8b4ef4a8324be2f465cd8e788983fa53
ws-sumo <129716148+ws-sumo@users.noreply.github.com>
2023-04-04 16:42:22 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ["/Secret/1"],
+  protectedRoutes: ["/共享文件/Secret/1"],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'wangshuo111@outlook.com',
